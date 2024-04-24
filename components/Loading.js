7222501ad581f68ea7c4c56eab4284cb4ea2a4f2
melissapennington <mlpennington9@gmail.com>
2024-04-23 20:38:29 -0500

    edit

--- components/Loading.js
+++ components/Loading.js
@@ -23,15 +23,5 @@ export default function Loading() {
       </div>
       <div className="spoke" />
     </div>
-  // <div className="text-center mt-5">
-  //   <Spinner
-  //     animation="border"
-  //     style={{
-  //       color: '#00BF67',
-  //       width: '100px',
-  //       height: '100px',
-  //     }}
-  //   />
-  // </div>
   );
 }
